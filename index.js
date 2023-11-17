8c06c09e88fabb95758f9cecbff55c33671e8389
Ana Holmen <aholmen@Anas-MacBook-Air.local>
2023-11-17 17:44:43 -0600

    edited some code.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //1.Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93//
+
 // 1a. Programmatically subtract the value of the first element in the array from the value in the last element of the array.//
 
-// Decalare varible for ages, add the sample numers, then console log the ages. then minus ages -1 and console log it, to get 90//
+// Decalare varible for ages, add the sample numbers, then console log the ages. then minus ages -1 and console log it, to get 90//
 let ages = [3, 9, 23, 64, 2, 8, 28, 93];
 console.log("Ages:", ages);
 let minusAge = ages[ages.length - 1] - ages[0];
@@ -9,10 +10,10 @@ console.log("minusAge", minusAge);
 
 // 1b. Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).//
 
-//  Use push method with age varible and push new age value 100, then console new age value, then decalare new minusagepush and console log minusagepush//
+//  Use push method with age varible and push new age value 100, then console new age value, then decalare new minusAgePush and console log minusAgePush//
 
 ages.push(100);
-console.log("Ages after pushing a new value", ages);
+console.log("Ages after pushing a new value:", ages);
 let minusAgePush = ages[ages.length - 1] - ages[0];
 console.log("minusAge", minusAgePush);
 
@@ -35,8 +36,10 @@ let average = sumOfAges / ages.length;
 console.log("average", average);
 
 // 2 Create an array called names that contains the following values: 'Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'.//
+
 //2a. Use a loop to iterate through the array and calculate the average number of letters per name. //
-// Declare new var with names, then declare totalChars set to 0, then create loop then set totalChar += name[i]length, then console and then divide charstotal/names.length//
+
+// Declare new var with names, then declare totalChars set to 0, then create loop then set totalChar += name[i]length, then console and then divide totalChars/names.length//
 
 let names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
 let totalChars = 0;
@@ -50,6 +53,8 @@ console.log("Average of Names", AverageName);
 
 // 2b. Use a loop to iterate through the array again and concatenate all the names together, separated by spaces.//
 
+// Declare new varible concat names, make 4 loop , set concatNames = concatNames.concat(names[i] + " "); then console.log it//
+
 let concatNames = "";
 for (let i = 0; i < names.length; i++) {
   concatNames = concatNames.concat(names[i] + " ");
@@ -57,11 +62,14 @@ for (let i = 0; i < names.length; i++) {
 }
 
 // 3. How do you access the last element of any array? //
-// ages[ages.length - 1]); was used to access the last element so we use that to acress the last in the array//
+
+// ages[ages.length - 1]); was used to access the last element so we use that to access the last in the array//
+
 console.log("last element of age array;", ages[ages.length - 1]);
 
 // 4. How do you access the first element of any array //
-// ages[0]); was used to access the first element so we use that to acress the first in the array//
+
+// ages[0]); was used to access the first element so we use that to access the first in the array//
 
 console.log("First element of ages array;", ages[0]);
 
@@ -79,6 +87,7 @@ for (let i = 0; i < names.length; i++) {
 }
 
 // 6. Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array.//
+
 //Name new varible charsTotal, make a 4loop ,  reference charTotal += name.length[i] then console.log charsTotal//
 
 let charsTotal = 0;
@@ -92,7 +101,7 @@ for (let i = 0; i < nameLengths.length; i++) {
 //n number of times. (i.e. if I pass in 'Hello' and 3, I would expect the function to return 'HelloHelloHello').//
 
 // Declare function concatwords wtih two arguments word and n and pass in hello and 3 wwith concat word. Then console.log  "Word par "and n
-//and  declare let concat = word. repeat(n))and console log concat to get hello 3 times//
+//and declare let concat = word. repeat(n))and console log concat to get hello 3 times//
 //
 
 function concatWords(word, n) {
@@ -108,7 +117,7 @@ concatWords("hello", 3);
 // Create function full name with param first and full name and then use let full name = and concat first and full name then console log full name//
 
 function fullName(firstName, lastName) {
-  let fullName = firstName + "" + lastName;
+  let fullName = firstName + " " + lastName;
   console.log(fullName);
 }
 
@@ -123,7 +132,7 @@ let numbers1 = [100, 200, 300, 400];
 let numbers2 = [1, 2, 3, 4];
 
 // Create a function with the sum of the numbers array, have let total= o  and created a loop,//
-// make a if else statment to make sure it returns true if the array is greater than 100//
+// make a, if else statement to make sure it returns true if the array is greater than 100//
 
 function sumNumbersArray(array) {
   let total = 0;
@@ -146,6 +155,8 @@ sumNumbersArray(numbers1);
 
 // 10. Write a function that takes an array of numbers and returns the average of all the elements in the array.//
 
+// Declare new function calculateNumbersAverage(array), then let total = o then create a 4 loop, then have total/array.length and have it return the average.
+
 function calculateNumbersAverage(array) {
   let total = 0;
 
@@ -165,7 +176,7 @@ calculateNumbersAverage(numbers1);
 
 // First create to functions number 3 and number 4. then declare a function with twoAverage with both array, then console log parameter and then arrays 1 and 2/
 // then declare the 2 total= 0 then make a loop, then  do a second loop for the second array//
-// then figure out the average for both arraays then console.log and average them.
+// then figure out the average for both arrays then console.log and average them.
 
 let number3 = [100, 100, 100];
 let number4 = [100, 100, 99];
